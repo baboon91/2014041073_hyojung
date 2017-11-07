1bc588f04e60ce79c57db8b62658deec57490cf3
baboon91 <32186793+baboon91@users.noreply.github.com>
2017-11-07 12:34:03 +0900

    Add files via upload

---
+++ 10.27/ExceptionExist.java
@@ -0,0 +1,20 @@
+
+public class ExceptionExist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		func();
+		
+	}
+
+	static void func() 
+	{
+		int i = 1;
+		int j = 1;
+		System.out.println(i/j);   
+	}
+
+	
+	
+}
